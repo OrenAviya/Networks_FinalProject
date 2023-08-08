3a88a45e3c0aa1b85f49c6863751e51b653271c6
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2023-08-08 11:23:29 +0300

    גם שומר קובץ של הפאקטות וגם גרפים

--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,10 @@
 import time
 from scapy.all import sniff, IP, TCP, IPv6
+import re
+import os
+
+# Define the path to the folder where you want to save the plot images
+output_folder = "c:/Users/97258/OneDrive - Ariel University/year 2/semester b/Communication networks/פרוייקט סיום"
 
 # Define global variables to store message information
 message_groups = {}  # Dictionary to hold groups of messages
@@ -8,7 +13,7 @@ message_counter = 0  # Counter to keep track of messages
 # Function to check if packets belong to the same group
 def are_packets_in_same_group_first(packet1, packet2):
     # Define threshold values for time and size similarity (you can adjust these as needed)
-    time_threshold = 0.5  # seconds
+    time_threshold = 0.2 # seconds
     size_threshold = 16  # bytes
 
     # Check if packets have the same size and close transmission times
@@ -42,16 +47,8 @@ def process_packet(packet):
             timestamp = time.time()
             size = len(packet)
 
-    # if packet.haslayer(TCP) and packet.haslayer(IP):
-
-    #     # Extract the source IP, destination IP, timestamp, and size
-    #     src_ip = packet[0].getlayer(IP).src
-    #     dst_ip = packet[0].getlayer(IP).dst
-    #     timestamp = time.time()
-    #     size = len(packet)
-
     # Create a unique identifier for the message group
-    group_identifier = f"{src_ip}_{dst_ip}"
+    group_identifier = f"{src_ip}--{dst_ip}"
 
     # Check if this is a new group or an existing one
     if group_identifier not in message_groups:
@@ -77,28 +74,43 @@ def process_packet(packet):
 # Start sniffing
 try:
     # Sniff the network for a limited number of packets (modify count as needed)
-    sniff(filter="tcp port 443 or tcp port 80 ", prn=process_packet, timeout=60)
+    sniff(filter="tcp port 443 or tcp port 80", prn=process_packet, timeout=20)
 
-    # Process and plot the data for each message group
-    for group_identifier, messages in message_groups.items():
-        if group_identifier == "2a02:6680:2105:aa78:5d79:df94:5095:14ea_2a03:2880:f242:1c2:face:b00c:0:167" or group_identifier=="2a03:2880:f242:1c2:face:b00c:0:167_2a02:6680:2105:aa78:5d79:df94:5095:14ea":
-            # Extract information for plotting
-            message_ids = [msg[0] for msg in messages]
-            message_sizes = [msg[1] for msg in messages]
-            timestamps = [msg[2] for msg in messages]
+    # Open the file in append mode
+    with open("c:/Users/97258/OneDrive - Ariel University/year 2/semester b/Communication networks/פרוייקט סיום/output-massages1.txt", "a") as file:
+        # Process and plot the data for each message group
+        for group_identifier, messages in message_groups.items():
+            if group_identifier == "2a02:6680:2105:aa78:5d79:df94:5095:14ea--2a03:2880:f242:1c2:face:b00c:0:167" or group_identifier=="2a03:2880:f242:1c2:face:b00c:0:167--2a02:6680:2105:aa78:5d79:df94:5095:14ea":
+            
+                # Remove colons from group identifier to create a valid filename
+                sanitized_group_identifier = re.sub(r'[:]', '_', group_identifier)
 
-            # Now you can use any plotting library (e.g., matplotlib) to visualize the data
-            # Here's a basic example using matplotlib:
-            import matplotlib.pyplot as plt
-                    
-            # Plot inter-message delays as a column chart
-            plt.figure(figsize=(10, 5))
-            plt.bar(timestamps, message_sizes, width=0.1, align='center', color='blue', edgecolor='black')
-            plt.xlabel("Timestamp")
-            plt.ylabel("Message Size (bytes)")
-            plt.title(f"Message Sizes over Time for Group: {group_identifier}")
-            plt.grid(axis='y', linestyle='--', alpha=0.7)
-            plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
-            plt.show()
+                # Extract information for plotting
+                message_ids = [msg[0] for msg in messages]
+                message_sizes = [msg[1] for msg in messages]
+                timestamps = [msg[2] for msg in messages]
+
+                # Append data to the file
+                file.write(f"Group Identifier: {group_identifier}\n")
+                for msg_id, msg_size, timestamp in zip(message_ids, message_sizes, timestamps):
+                    file.write(f"Message ID: {msg_id}, Size: {msg_size} bytes, Timestamp: {timestamp}\n")
+
+                # Now you can use any plotting library (e.g., matplotlib) to visualize the data
+                # Here's a basic example using matplotlib:
+                import matplotlib.pyplot as plt
+                        
+                # Plot inter-message delays as a column chart
+                plt.figure(figsize=(10, 5))
+                plt.bar(timestamps, message_sizes, width=0.1, align='center', color='blue', edgecolor='black')
+                plt.xlabel("Timestamp")
+                plt.ylabel("Message Size (bytes)")
+                plt.title(f"Message Sizes over Time for Group: {group_identifier}")
+                plt.grid(axis='y', linestyle='--', alpha=0.7)
+                plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+                plot_filename = os.path.join(output_folder, f"{sanitized_group_identifier}_Channel1.png")
+                plt.savefig(plot_filename)
+                plt.show()
+
+
 except KeyboardInterrupt:
     print("Sniffing stopped.")
